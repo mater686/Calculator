819f782226ea2aebf7cdcaf801d310fda40e0edb
mater686 <121267965+mater686@users.noreply.github.com>
2023-01-20 08:49:26 -0800

    Update main.py
    Now has percentage

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import math
 mat = input("+ - / * sqrt more:")
 if mat == "more":
-    mat = input("area, circum, log: ")
+    mat = input("area, circum, log, percentage: ")
     if mat == "area":
         mat = (input("circle, triangle: "))
         if mat == "circle":
@@ -18,6 +18,10 @@ if mat == "more":
         base = int(input("Base: "))
         exponent = int(input("Exponent: "))
         print(math.log(base**exponent) / math.log(base))
+    if mat == "percentage":
+        percent = int(input("Percent of number: "))
+        number = int(input("Number: "))
+        print(percent * number / 100)
 else:
     if mat == "sqrt":
         numbersqrt = int(input("Numbersqrt:"))
